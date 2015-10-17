e2c47510fd732025d480c946e63228660a6e1b20
Nickolay <lukaville@gmail.com>
2015-10-17 20:18:30 +0300

    more arguments

--- src/main/java/com/lukaville/server/model/Preferences.java
+++ src/main/java/com/lukaville/server/model/Preferences.java
@@ -8,24 +8,31 @@ import org.apache.commons.cli.*;
 public class Preferences {
     public static final String ARG_ROOT_DIRECTORY = "r";
     public static final String ARG_CPU_NUMBER = "c";
+    public static final String ARG_HOST = "h";
+    public static final String ARG_PORT = "p";
 
-    private static String host = "0.0.0.0";
-    private static int port = 5555;
+    private static String host;
+    private static int port;
     private static String directory;
     private static int cpuNumber;
 
     private static Options options = new Options();
+
     static {
         options.addOption(new Option(ARG_ROOT_DIRECTORY, true, "Root directory"));
         options.addOption(new Option(ARG_CPU_NUMBER, true, "CPU number"));
+        options.addOption(new Option(ARG_HOST, true, "Host"));
+        options.addOption(new Option(ARG_PORT, true, "Port"));
     }
 
     public static void fromArguments(String[] args) throws ParseException {
         CommandLineParser parser = new DefaultParser();
 
         CommandLine commandLine = parser.parse(options, args);
-        directory = commandLine.getOptionValue(ARG_ROOT_DIRECTORY);
-        cpuNumber = Integer.parseInt(commandLine.getOptionValue(ARG_CPU_NUMBER));
+        directory = commandLine.getOptionValue(ARG_ROOT_DIRECTORY, "www");
+        cpuNumber = Integer.parseInt(commandLine.getOptionValue(ARG_CPU_NUMBER, "8"));
+        host = commandLine.getOptionValue(ARG_HOST, "0.0.0.0");
+        port = Integer.parseInt(commandLine.getOptionValue(ARG_PORT, "5555"));
     }
 
     public static void printHelp() {
